b11a7bee2a2ad8722010834a1f492355d5b84828
GeneralAverage <lin_m@delbarton.org>
2025-04-06 11:47:06 -0400

    Update groqai.py

--- groqai.py
+++ groqai.py
@@ -5,16 +5,20 @@ apikey = "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"
 client = Groq(api_key=apikey)
 
 
-
+chathistory = ""
 while True:
     message = input("what do u wanna tell the ai? ")
+    chathistory = chathistory + ("   USER: " + message)
     chat_completion = client.chat.completions.create(
     messages=[
         {
             "role": "system",
             "content": "Talk like a rival. Be somewhat condescending, but still friends with the user.",
         },
-
+        {
+            "role": "assistant",
+            "content": "Here is the history of the current conversation: " + chathistory,
+        }, 
         {
             "role": "user",
             "content": message,
@@ -24,4 +28,5 @@ while True:
     max_completion_tokens=1024,
     stream=False,
     )
+    chathistory = chathistory + ("   AI: " + chat_completion.choices[0].message.content)
     print(chat_completion.choices[0].message.content)
