11b702bfdbe0689fcfa0ea1e78fe3f5a0d956dcd
GeneralAverage <lin_m@delbarton.org>
2025-04-06 11:29:41 -0400

    added conversation

--- groqai.py
+++ groqai.py
@@ -1,15 +1,27 @@
 from groq import Groq
 
-api-key = "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"
+apikey = "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"
 
-client = Groq(api_key = api-key)
-chat_completion = client.chat.completions.create(
-    {
-        "role" : "system",
-        "message": ""
-    },
-    {
-        "role" : "user",
-        "message": ""
-    }
-)
+client = Groq(api_key=apikey)
+
+
+
+while True:
+    message = input("what do u wanna tell the ai? ")
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "Talk like a rival. Be somewhat condescending, but still friends with the user.",
+        },
+
+        {
+            "role": "user",
+            "content": message,
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+    max_completion_tokens=1024,
+    stream=False,
+    )
+    print(chat_completion.choices[0].message.content)
